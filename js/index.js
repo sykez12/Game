48b9284602af2584245ed485601ec220f7c4ed39
sykez12 <adam_paddy@hotmail.co.uk>
2020-11-06 11:58:12 +0000

    changes

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ let good;
 let compTurn;
 let intervalId;
 let noise = true;
-let on = false;
+let on = true;
 let win;
 
 const turnCounter = document.querySelector("#turn");
@@ -16,13 +16,48 @@ const topLeft = document.querySelector("#topleft");
 const topRight = document.querySelector("#topright");
 const bottomLeft = document.querySelector("#bottomleft");
 const bottomRight = document.querySelector("#bottomright");
-const strictButton = document.querySelector("#strict");
-const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
 
-//Buttons
+//Button
 
+startButton.addEventListener('click', (event) => {
+    play();
+});
 
+function play() {
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true;
+    for (var i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() *4) +1);
+    }
+    compTurn = true;
 
+    intervalId = setInterval(gameTurn, 800);
+}
 
+function gameTurn() {
+    on = false;
+
+    if (flash == turn) {
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] == 1) one();
+            if (order[flash] == 2) two();
+            if (order[flash] == 3) three();
+            if (order[flash] == 4) four();
+        }, 200)
+    }
+}
 
